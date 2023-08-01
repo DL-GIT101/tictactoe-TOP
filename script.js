8a7d2ad75d57bbef015948c085c862f70160424e
donlHenard <pastor.git101@gmail.com>
2023-08-01 23:32:50 +0800

    Add module and fn
    Cell suppose to be factory but it must be initizlied before the module
    so i made it to only function

---
+++ script.js
@@ -0,0 +1,37 @@
+const GameBoard = (() => {
+
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+
+    for( let i = 0 ; i < rows ; i++){
+        board[i] = [];
+
+        for(let j = 0; j < columns; j++){
+            board[i].push(Cell());
+        }
+    }
+
+   const getBoard = () => board;
+
+    return {
+        getBoard
+    };
+
+})();
+
+function Cell() {
+
+    let value = "0";
+
+    const move = (player) => {
+        value = player;
+    }
+
+    const getValue = () => value;
+
+    return { 
+        move, 
+        getValue
+    };
+};
