aa6418a2aa4cda571defcc1b93dd4c4c86ab94e9
donlHenard <pastor.git101@gmail.com>
2023-08-03 20:10:35 +0800

    Change 2d array to 1d

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 const GameBoard = (() => {
 
-    const rows = 3;
-    const columns = 3;
+    const cellCount = 9;
     const board = [];
 
-    for( let i = 0 ; i < rows ; i++){
-        board[i] = [];
-
-        for(let j = 0; j < columns; j++){
-            board[i].push(Cell());
-        }
+    for( let i = 0 ; i < cellCount ; i++){
+        board.push(Cell());
     }
 
    const getBoard = () => board;
@@ -17,11 +12,8 @@ const GameBoard = (() => {
    const displayBoard = () => {
         const mainBoard = document.querySelector('main');
 
-            board.forEach(cells => {
+            board.forEach(cell => {
 
-                cells.forEach(cell => {
-                    
-                
                 let grid = document.createElement('div');
                 grid.className = "cell";
 
@@ -32,7 +24,6 @@ const GameBoard = (() => {
 
                 mainBoard.appendChild(grid);
             });
-        });
    };
 
     return {
