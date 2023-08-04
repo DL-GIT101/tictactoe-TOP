4e4cb716771408b1a3a8c59963b1e0abe24bb035
donlHenard <pastor.git101@gmail.com>
2023-08-04 21:20:52 +0800

    Add fn on cell

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 const Cell = () => {
 
+    let value = 1;
+
+    const changeValue = (token) => {
+        value = token;
+    }
+
+    const getValue = () => value;
+
+    return {
+        changeValue,getValue
+    };
 };
 
